dc2b4fe3992abeddf71f014c1d657c64d5ec9573
Seongyeol Yun <02tjdduf@gmail.com>
2022-09-14 17:14:27 +0900

    DrawGrid 추가

---
+++ Homework04/DrawGrid.py
@@ -0,0 +1,109 @@
+import turtle
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count +1
+
+turtle.left(90)
+count = 0
+
+while(count< 5):
+    turtle.forward(100)
+    count = count +1
+
+turtle.left(90)
+
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.left(90)
+count = 0
+
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+    
+turtle.left(90)
+count = 0
+
+turtle.goto(0,100)
+while(count < 5):
+    turtle.forward(100)
+    count = count +1
+turtle.penup()
+turtle.goto(0,200)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count +1
+
+
+turtle.penup()
+turtle.goto(0,300)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.penup()
+turtle.goto(0,400)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.penup()
+turtle.goto(0,500)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+turtle.penup()
+turtle.home()
+turtle.goto(100,0)
+turtle.pendown()
+count = 0
+turtle.left(90)
+
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.penup()
+turtle.goto(200,0)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.penup()
+turtle.goto(300,0)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+
+turtle.penup()
+turtle.goto(400,0)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count +1
+
+turtle.penup()
+turtle.goto(500,0)
+turtle.pendown()
+count = 0
+while(count < 5):
+    turtle.forward(100)
+    count = count + 1
+    
