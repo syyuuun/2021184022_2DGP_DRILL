c96587e4245de2acdc2bdbaf76f9aad3048ec103
Seongyeol Yun <02tjdduf@gmail.com>
2022-09-21 17:40:18 +0900

    ReectAndCircle.py 수정

--- Homework06/RectAndiCircle.py
+++ Homework06/RectAndiCircle.py
@@ -6,9 +6,8 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 
-
 flag = 0
-move_x = 0
+move_x = 400
 move_y = 90
 direction = 0
 degree = 0
@@ -40,15 +39,20 @@ while(True):
             if(move_y >= 600 & move_x <= 0):
                 move_y = move_y - 2
                 move_x = move_x
+                if(move_y ==90):
+                    move_x = 400
+                    move_y = 90
+                    flag =1
             else:
                 direction = 0
-                flag = 1
     elif(flag ==1):
+        pos_x = 400
+        pos_y = 90
         clear_canvas_now()
         grass.draw_now(400,30)
-        move_x = math.cos(degree / 360 * 2 * math.pi) * 200 + 400
-        move_y = math.sin(degree / 360 * 2 * math.pi) * 200 + 150
-        character.draw_now(move_x,move_y)
+        pos_x = math.cos(degree / 360 * 2 * math.pi) * 200 + 400
+        pos_y = math.sin(degree / 360 * 2 * math.pi) * 200 + 300
+        character.draw_now(pos_x,pos_y)
         degree = degree + 1
         if(degree > 360):
             flag = 0
