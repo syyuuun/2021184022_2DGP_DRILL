ab86cf9a059a9e90e27e70e531ec6e442ce2ec4b
Seongyeol Yun <02tjdduf@gmail.com>
2022-10-02 18:16:00 +0900

    상하좌우 움직임 구현

--- Homework08/drill08.py
+++ Homework08/drill08.py
@@ -1,54 +1,59 @@
 from pico2d import *
-
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
-
-
-
-def handle_events():
-   global running
-   global x,y
-   global events
-   global dir
-   events = get_events()
-   for event in events:
-       if event.type == SDL_QUIT:
-           running = False
-       elif event.type == SDL_KEYDOWN:
-           if event.key == SDLK_ESCAPE:
-               running = False
-           if event.key == SDLK_RIGHT:
-               dir += 1
-               x += 10
-           elif event.key == SDLK_LEFT:
-               dir -= 1
-               x -= 10
-           elif event.key == SDLK_UP:
-               y += 10
-           elif event.key == SDLK_DOWN:
-               y -= 10
-
-open_canvas(TUK_WIDTH,TUK_HEIGHT)
-tuk_ground = load_image("TUK_GROUND.png")
-character = load_image("animation_sheet.png")
+open_canvas()
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
 
 running = True
-x,y = TUK_WIDTH // 2, TUK_HEIGHT //2
-frame = 0
-dir = 0
+player_x = 400
+player_y = 90
+frame_bottom = 0
+frame =0
+dir_x = 0
+dir_y = 0
+def handle_events():
+    global running
+    global dir_x
+    global dir_y
+    global frame_bottom
+    global player_y
+    events = get_events()
 
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_LEFT:
+                dir_x -=1
+                frame_bottom = 0
+            elif event.key == SDLK_RIGHT:
+                dir_x +=1
+                frame_bottom = 100
+            elif event.key == SDLK_UP:
+                dir_y +=1
+            elif event.key == SDLK_DOWN:
+                dir_y -=1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_LEFT:
+                dir_x +=1
+                frame_bottom = 200
+            elif event.key == SDLK_RIGHT:
+                dir_x -=1
+                frame_bottom = 300
+            elif event.key == SDLK_UP:
+                dir_y -=1
+            elif event.key == SDLK_DOWN:
+                dir_y +=1
 while running:
-    clear_canvas()
-    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT //2)
-    if dir == -1:
-        character.clip_draw(frame * 800, 400, 100, 100, x, y)
-    elif dir == 1:
-        character.clip_draw(frame * 800, 200, 100, 100, x, y)
-    elif dir == 0:
-        character.clip_draw(frame * 800, 100, 100, 100, x, y)
-    update_canvas()
     handle_events()
+    clear_canvas()
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    character.clip_draw(frame*100,frame_bottom,100,100,player_x,player_y)
+    player_x += dir_x*5
+    player_y += dir_y*5
     frame = (frame + 1) % 8
-    x += dir * 2
-    delay(0.01)
-
+    delay(0.05)
+    update_canvas()
 close_canvas()
